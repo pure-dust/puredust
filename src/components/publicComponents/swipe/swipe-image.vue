018dc2eb38c76b831b8fed799a6310e80712995b
pure-dust <932184220@qq.com>
2020-03-12 11:08:00 +0800

    test

--- src/components/publicComponents/swipe/swipe-image.vue
+++ src/components/publicComponents/swipe/swipe-image.vue
@@ -28,7 +28,7 @@ export default {
   methods: {
     rotate() {
       let temp = this.$refs.img
-      temp.style.backgroundImage = 'url(http://127.0.0.1:5050/' + this.list[0] + ')'
+      temp.style.backgroundImage = this.list[0]
 
       if (this.clock != null) return;
       else {
@@ -37,7 +37,7 @@ export default {
           else {
             this.currentId++;
           }
-            temp.style.backgroundImage = 'url(http://127.0.0.1:5050/' + this.list[this.currentId] + ')'
+            temp.style.backgroundImage = this.list[this.currentId]
         }, this.time);
       }
     }
